9b4d9443f08428b04a723223799aa461dcc769e8
Alexandru Carp <alexandrucarp02@gmail.com>
2024-04-23 10:28:32 +0300

    todos

--- server/server.py
+++ server/server.py
@@ -20,3 +20,16 @@ def button_clicked():
 
 if __name__ == '__main__':
     socketio.run(app, host='192.168.1.5', port=5000)
+
+
+# TODO
+
+# Prima pagina in unity, in care se alege nr de jucatori, cand se apasa new game se face socketio.emit catre server (cu nr
+# de jucatori si codul)
+# Urmatoarea pagina in unity va afisa cati jucatori s au conectat (asteapta sa primeasca socketio.emit de la server 
+# ca sa modifice numarul)
+
+# Serverul afiseaza pagina initiala in care se introduce codul ca pagina de start (in '/')
+# Cand se apasa submit, serverul verifica codul si numarul de jucatori deja conectati. Daca e ok, redirectioneaza spre pagina
+# de alegere barca etc (momentan doar o pagina cu un text care zice ca s a conectat cu succes), altfel afiseaza un text de eroare
+# pe pagina de start
